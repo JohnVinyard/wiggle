26ac2c020b158af424dcbb0a0d6f42b37b02027b
John Vinyard <johnwho@gmail.com>
2024-05-16 13:50:32 -0500

    Add get_synth to exports

--- wiggle/__init__.py
+++ wiggle/__init__.py
@@ -8,5 +8,5 @@ from .samplerparams import \
     SamplerParameters, ReverbParameters, GainParameters, GainKeyPoint, \
     FilterParameters
 from .fetch import AudioFetcher
-from .synths import list_synths, get_synth_by_id, get_synth_by_name, render
+from .synths import list_synths, get_synth_by_id, get_synth_by_name, get_synth, render
 from .basesynth import write_samples, encode_samples
